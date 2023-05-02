58c4ceba7de7c74b72606d5bccf795123e0df191
kmiono <86765332+kmiono@users.noreply.github.com>
2023-05-02 09:30:59 +0900

    タグ機能追加

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from "../lib/constants";
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1>Hello!</h1>
+      <h1>Blog.</h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
         <a
