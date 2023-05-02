d86eca894e3d6fb51a8cca7872becd7300d6b13c
kmiono <86765332+kmiono@users.noreply.github.com>
2023-05-02 11:00:24 +0900

    タグ一覧ページ実装

---
+++ pages/tags/[tag].tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import Post from "../../interfaces/post";
+import Container from "../../components/container";
+import MoreStories from "../../components/more-stories";
+import { getAllTags, getPostsByTag } from "../../lib/api";
+
+type Props = {
+  posts: Post[];
+  tag: string;
+};
+
+export default function Index({ posts, tag }: Props) {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Tag:{tag}</title>
+        </Head>
+        <Container>
+          <MoreStories posts={posts} />
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+type Params = {
+  params: {
+    tag: string;
+  };
+};
+
+export const getStaticProps = ({ params }: Params) => {
+  const posts = getPostsByTag(params.tag, [
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+    "tags",
+  ]);
+
+  return {
+    props: {
+      posts: posts,
+      tag: params.tag,
+    },
+  };
+};
+
+export function getStaticPaths() {
+  const tags = getAllTags();
+
+  return {
+    paths: tags.map((tag) => {
+      return {
+        params: {
+          tag: tag,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
